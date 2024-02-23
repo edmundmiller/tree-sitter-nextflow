198d366b1b62c0f31052dba5a45abefab3b46acc
Edmund Miller <git@edmundmiller.dev>
2024-02-22 22:11:38 -0600

    chore: Copy TODOs

--- grammar.js
+++ grammar.js
@@ -2,7 +2,49 @@ module.exports = grammar({
     name: "Nextflow",
 
     rules: {
-        // TODO: add the actual grammar rules
-        source_file: ($) => "hello",
+        source_file: ($) => repeat($._definition),
+
+        _definition: ($) =>
+            choice(
+                $.function_definition,
+                // TODO: other kinds of definitions
+            ),
+
+        function_definition: ($) =>
+            seq("func", $.identifier, $.parameter_list, $._type, $.block),
+
+        parameter_list: ($) =>
+            seq(
+                "(",
+                // TODO: parameters
+                ")",
+            ),
+
+        _type: ($) =>
+            choice(
+                "bool",
+                // TODO: other kinds of types
+            ),
+
+        block: ($) => seq("{", repeat($._statement), "}"),
+
+        _statement: ($) =>
+            choice(
+                $.return_statement,
+                // TODO: other kinds of statements
+            ),
+
+        return_statement: ($) => seq("return", $._expression, ";"),
+
+        _expression: ($) =>
+            choice(
+                $.identifier,
+                $.number,
+                // TODO: other kinds of expressions
+            ),
+
+        identifier: ($) => /[a-z]+/,
+
+        number: ($) => /\d+/,
     },
 });
