67b26caa4c47fd099a0d3b994b6c9af11bf0cf25
Edmund Miller <git@edmundmiller.dev>
2024-12-27 10:43:03 -0600

    feat: Add initial Nextflow grammar with basic declarations and comments

--- grammar.js
+++ grammar.js
@@ -10,8 +10,100 @@
 module.exports = grammar({
   name: "nextflow",
 
+  extras: $ => [
+    /\s/,
+    $.comment
+  ],
+
   rules: {
-    // TODO: add the actual grammar rules
-    source_file: $ => "hello"
+    source_file: $ => repeat(choice(
+      $.comment,
+      $.shebang,
+      $.feature_flag,
+      $.include,
+      $.parameter,
+      $.workflow,
+      $.process,
+      $.function,
+      $.enum_type,
+      $.output_block,
+      $.statement
+    )),
+
+    // Comments
+    comment: $ => choice(
+      seq('//', /.*/),
+      seq(
+        '/*',
+        /[^*]*\*+([^/*][^*]*\*+)*/,
+        '/'
+      )
+    ),
+
+    // Top-level declarations
+    shebang: $ => seq(
+      '#!',
+      /.*/
+    ),
+
+    feature_flag: $ => seq(
+      'nextflow',
+      '.',
+      $.identifier,
+      '.',
+      $.identifier,
+      '=',
+      choice($.number, $.string, $.boolean)
+    ),
+
+    include: $ => seq(
+      'include',
+      '{',
+      commaSep1($.include_item),
+      '}',
+      'from',
+      $.string
+    ),
+
+    include_item: $ => seq(
+      $.identifier,
+      optional(seq('as', $.identifier))
+    ),
+
+    parameter: $ => seq(
+      'params',
+      '.',
+      $.identifier,
+      '=',
+      $._expression
+    ),
+
+    // Basic expressions
+    _expression: $ => choice(
+      $.identifier,
+      $.number,
+      $.string,
+      $.boolean
+    ),
+
+    identifier: $ => /[a-zA-Z_][a-zA-Z0-9_]*/,
+    
+    number: $ => choice(
+      /\d+/,
+      /\d+\.\d+/
+    ),
+
+    string: $ => choice(
+      seq('"', /[^"]*/, '"'),
+      seq("'", /[^']*/, "'"),
+      seq('"""', /[^"]*/, '"""'),
+      seq("'''", /[^']*/, "'''")
+    ),
+
+    boolean: $ => choice('true', 'false')
   }
 });
+
+function commaSep1(rule) {
+  return seq(rule, repeat(seq(',', rule)))
+}
